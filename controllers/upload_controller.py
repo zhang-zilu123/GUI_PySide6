6cbdd9f44494ce7ca7152b2bba54b4ca1cdb9543
alumni77 <2112416672@qq.com>
2025-09-11 15:05:32 +0800

    运行完整流程的模型

--- controllers/upload_controller.py
+++ controllers/upload_controller.py
@@ -59,32 +59,32 @@ class ExtractDataWorker(QThread):
         os.environ['MINERU_MODEL_SOURCE'] = 'local'
         print(f'开始解析PDF文件: {file_paths}')
         # 解析pdf
-        # start_time = time.time()
-        # local_md_dirs = parse_doc(path_list=file_paths, output_dir="./output", backend="pipeline")
-        # end_time = time.time()
-        # print(f"PDF解析完成，耗时 {end_time - start_time:.2f} 秒")
+        start_time = time.time()
+        local_md_dirs = parse_doc(path_list=file_paths, output_dir="./output", backend="pipeline")
+        end_time = time.time()
+        print(f"PDF解析完成，耗时 {end_time - start_time:.2f} 秒")
         # local_md_dirs = ['./output\\746账单\\auto', './output\\G25RU01070-4A费用明细\\auto',
         #                  './output\\G25RU03088费用明细\\auto', './output\\G25RU05039费用明细\\auto']
 
-        # OUTPUT_DIR = Path(__file__).resolve().parents[1] / "output"
-        # corrector = TableCorrector(API_KEY)
-        # corrector.process_directory(OUTPUT_DIR)
-
-        # start_time = time.time()
-        # info_dict = {}
-        # for local_md_dir in local_md_dirs:
-        #     path_parts = local_md_dir.replace('\\', '/').split('/')
-        #     output_index = path_parts.index('output')
-        #     file_name = path_parts[output_index + 1]
-        #     md_path = os.path.join(local_md_dir, f"{file_name}.corrected.md")
-        #     temp = extract_info_from_md(md_path)
-        #     info_dict[file_name] = json.loads(temp).get("费用明细", [])
-        # end_time = time.time()
-        # print(f"纠正和提取结构，耗时 {end_time - start_time:.2f} 秒")
+        OUTPUT_DIR = Path(__file__).resolve().parents[1] / "output"
+        corrector = TableCorrector(API_KEY)
+        corrector.process_directory(OUTPUT_DIR)
 
         start_time = time.time()
-        # info_dict = translate_json(info_dict)
-        info_dict = translate_json(get_data())
+        info_dict = {}
+        for local_md_dir in local_md_dirs:
+            path_parts = local_md_dir.replace('\\', '/').split('/')
+            output_index = path_parts.index('output')
+            file_name = path_parts[output_index + 1]
+            md_path = os.path.join(local_md_dir, f"{file_name}.corrected.md")
+            temp = extract_info_from_md(md_path)
+            info_dict[file_name] = json.loads(temp).get("费用明细", [])
+        end_time = time.time()
+        print(f"纠正和提取结构，耗时 {end_time - start_time:.2f} 秒")
+
+        start_time = time.time()
+        info_dict = translate_json(info_dict)
+        # info_dict = translate_json(get_data())
         end_time = time.time()
         print(f"翻译字段，耗时 {end_time - start_time:.2f} 秒")
         print('完成PDF文件解析', info_dict)
